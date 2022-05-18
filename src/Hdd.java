c68efa12f9933042e24cae6e70133435d72ac6bd
FedericoBianchiScuola <federico.bianchi@italessandrini.edu.it>
2022-05-18 12:28:20 +0200

    backup

---
+++ src/Hdd.java
@@ -0,0 +1,20 @@
+public class Hdd {
+    
+    private String marca;
+    private int rpm;
+    private double accesso;
+    private double capacita;
+
+
+    private final int punti_rpm = 1;
+    private final int punti_accesso= -200;
+    private final int punti_capacita= 500;
+    
+
+    public void leggiDati(int i)
+    {
+
+        
+
+    }
+}
